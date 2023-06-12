52987e2a032ede281150d59e3c3dddd90b50d171
JoséTS <josethomassanchezaguilar12@gmail.com>
2023-06-12 08:46:38 -0500

    Inicio de proyecto

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const happyForm = document.forms['happyForm'];
+happyForm.addEventListener('submit', (e) => {
+e.preventDefault();
+console.log(happyForm['number1']);
+console.log(happyForm['operator']); 
+console.log(happyForm['number2']);  
+});
