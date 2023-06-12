923fb882f8993a078914f562af7d2852ecadea9e
JoséTS <josethomassanchezaguilar12@gmail.com>
2023-06-12 09:55:37 -0500

    Solo falta la raíz

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
 const happyForm = document.forms['happyForm'];
+const result = document.getElementById('result');
+
 happyForm.addEventListener('submit', (e) => {
 e.preventDefault();
-console.log(happyForm['number1']);
-console.log(happyForm['operator']); 
-console.log(happyForm['number2']);  
-});
+const number1 = +happyForm['number1'].value;
+const operator = happyForm['operator'].value;
+const number2 = +happyForm['number2'].value;
+
+switch (operator)
+{
+  case '+':
+    value = number1 + number2;
+    break;
+  case '-':
+    value = number1 - number2;
+    break;
+  case '*':
+    value = number1 * number2;
+    break;
+  case '/':
+    value = number1 / number2;
+    break;
+  case '**':
+    value = number1 ** number2;
+    break;
+  case '**':
+    value = number1 ** (1/number2);
+    break;
+    default:
+    break;
+}
+result.textContent = value
+});                                    
